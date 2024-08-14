e4a890a3e8e9a77cce2aa787849b0a6486c11801
keaimk <fly1043@naver.com>
2024-08-14 16:57:32 +0900

    Char

---
+++ src/Ch02/lecture/p02tpye/C05Character.java
@@ -0,0 +1,27 @@
+package Ch02.lecture.p02tpye;
+
+public class C05Character {
+    public static void main(String[] args) {
+//        char (2byte)
+//        문자 하나 (작은 따옴표 사용)
+//        unicode
+        char a = 'a'; //
+        char b = '가'; //
+        char c = '한';
+        char d = '8';
+//        char e = 'gk';
+
+        char f = 65;
+        System.out.println("f = " + f);
+
+        char g = 45206;
+        System.out.println("g");
+
+        char h = 3743;
+        System.out.println("h " + h);
+
+        char i = '\u0E9F';
+        System.out.println("i = " + i);
+
+    }
+}
