82936444349e8d64bd1d5c4ea2c3c221f8594de9
keaimk <fly1043@naver.com>
2024-08-20 15:29:14 +0900

    대입 연산자

---
+++ src/Ch03/sec10/AssignmentOperatorExample.java
@@ -0,0 +1,17 @@
+package Ch03.sec10;
+
+public class AssignmentOperatorExample {
+    public static void main(String[] args) {
+        int result = 0;
+        result += 0;
+        System.out.println("result = " + result);
+        result -= 5;
+        System.out.println("result = " + result);
+        result *= 3;
+        System.out.println("result = " + result);
+        result /= 5;
+        System.out.println("result = " + result);
+        result %= 3;
+        System.out.println("result = " + result);
+    }
+}
