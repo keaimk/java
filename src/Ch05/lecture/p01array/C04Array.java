ea4ded1003e8bccb91c1e7a6a1ead81dfca2ec7f
keaimk <fly1043@naver.com>
2024-08-22 16:15:51 +0900

    array & for

--- src/Ch05/lecture/p01array/C04Array.java
+++ src/Ch05/lecture/p01array/C04Array.java
@@ -4,7 +4,7 @@ public class C04Array {
     public static void main(String[] args) {
         int[] a = new int[5];
 
-        a[0] = 3; // index
+        a[0] = 3;  // index
         a[1] = 33;
         a[2] = 99;
         a[3] = 101;
@@ -12,13 +12,14 @@ public class C04Array {
 
         System.out.println(a[0]);
         System.out.println(a[1]);
-        System.out.println(a[1]);
+        System.out.println(a[2]);
         System.out.println(a[3]);
         System.out.println(a[4]);
 
-        System.out.println("--------------------------");
+        System.out.println("--------------");
 
-        for (int i = 0; i < 5; i++) ;
-        System.out.println("i = " + i);
+        for (int i = 0; i < 5; i++) {
+            System.out.println(a[i]);
+        }
     }
 }
