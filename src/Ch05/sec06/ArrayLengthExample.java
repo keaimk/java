c2eb2b2b2be64f67579b854b4f2173f59634f90b
keaimk <fly1043@naver.com>
2024-08-22 16:28:04 +0900

    초기값과 길이 한 번에

---
+++ src/Ch05/sec06/ArrayLengthExample.java
@@ -0,0 +1,22 @@
+package Ch05.sec06;
+
+public class ArrayLengthExample {
+    public static void main(String[] args) {
+        int[] scores = {84, 90, 96};
+        int sum = 0;
+
+//        enhanced for로 바꿔보기
+//        for (int i = 0; i < scores.length; i++) {
+//            sum += scores[i];
+//        }
+
+        for (int score : scores) {
+            sum += score;
+        }
+        System.out.println("총합 = " + sum);
+
+        double avg = (double) sum / scores.length;
+        System.out.println("평균 = " + avg);
+
+    }
+}
