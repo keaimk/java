61b5ce18c614c81a43d2d950fa9ce736e098394f
keaimk <fly1043@naver.com>
2024-08-22 09:36:53 +0900

    확인문제 6

---
+++ src/Ch04/exercise/Solution6.java
@@ -0,0 +1,13 @@
+package Ch04.exercise;
+
+public class Solution6 {
+    public static void main(String[] args) {
+        String s = "";
+
+        for (int i = 0; i < 5; i++) {
+            s += "*";
+            System.out.println(s);
+        }
+
+    }
+}
