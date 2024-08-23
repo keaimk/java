b6926ac00221f08c856cc7557393aee7b4fc4256
keaimk <fly1043@naver.com>
2024-08-23 15:56:51 +0900

    Arrays

--- src/Ch05/sec06/ArrayCreateByValueListExample.java
+++ src/Ch05/sec06/ArrayCreateByValueListExample.java
@@ -2,19 +2,24 @@ package Ch05.sec06;
 
 public class ArrayCreateByValueListExample {
     public static void main(String[] args) {
+//        배열 번수 선언거=ㅓㅏ 배열 생성
         String[] season = {"Spring", "Summer", "Fall", "Winter"};
 
+//        배열 변수 선언과 배열 생성
         System.out.println("season[0] : " + season[0]);
         System.out.println("season[1] = " + season[1]);
         System.out.println("season[2] = " + season[2]);
         System.out.println("season[3] = " + season[3]);
 
+//        배열의 항목 값 읽기
         season[1] = "여름";
         System.out.println("season[1] : " + season[1]);
         System.out.println();
 
+//      배열 변수 선언과 배열 생성 
         int[] scores = {83, 90, 87};
 
+//        총 합과 평균 구하기
         int sum = 0;
         for (int i = 0; i < 3; i++) {
             sum += scores[i];
