6a35a46bd44c7f06af11208afc823d1abf3bea8c
keaimk <fly1043@naver.com>
2024-08-25 19:30:59 +0900

    확인문제 3

--- src/Ch04/exercise/Solution3.java
+++ src/Ch04/exercise/Solution3.java
@@ -1,4 +1,17 @@
 package Ch04.exercise;
 
 public class Solution3 {
+    public static void main(String[] args) {
+        int sum = 0;
+
+        int i = 1;
+        while (i <= 100) {
+            if ((i % 3) == 0) {
+                sum += i;
+            }
+            i++;
+        }
+
+        System.out.println("sum = " + sum);
+    }
 }
