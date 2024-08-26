29b3a27d2fdda815d1b4c6376d2f03f31eb0fd34
keaimk <fly1043@naver.com>
2024-08-26 15:06:33 +0900

    null

---
+++ src/Ch05/sec04/NullPointerExceptionExample.java
@@ -0,0 +1,9 @@
+package Ch05.sec04;
+
+public class NullPointerExceptionExample {
+    int[] intArray[] = null;
+    //intArray[0] = 10;
+
+    String str = null;
+    //System.out.println("총 문자 수: " + str.length() );
+}
