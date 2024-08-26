62bf507f81cd86fc2c635f0bd2365cd3b205a172
keaimk <fly1043@naver.com>
2024-08-26 09:42:00 +0900

    string

---
+++ src/Ch05/lecture/p04string/C01String.java
@@ -0,0 +1,13 @@
+package Ch05.lecture.p04string;
+
+public class C01String {
+    public static void main(String[] args) {
+        String a = new String("java");
+        String b = new String("java");
+
+        System.out.println("a = " + a);
+
+        System.out.println(a == b);
+    }
+}
+
