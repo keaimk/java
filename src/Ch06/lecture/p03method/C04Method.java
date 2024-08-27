40885fa85bf930fdd0ecc5fcb0283d5dd5c1678a
keaimk <fly1043@naver.com>
2024-08-27 09:56:39 +0900

    method

---
+++ src/Ch06/lecture/p03method/C04Method.java
@@ -0,0 +1,18 @@
+package Ch06.lecture.p03method;
+
+public class C04Method {
+    public static void main(String[] args) {
+        Myclass04 o1 = new Myclass04();
+        o1.method1();
+    }
+}
+
+class Myclass04 {
+    public void method1() {
+        System.out.println("코드들 ...");
+    }
+
+    public void method2() {
+        System.out.println("다른 코드들 ...");
+    }
+}
