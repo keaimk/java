647ad091f92ddf5c623c4e8e57fdc6ae46ed3324
keaimk <fly1043@naver.com>
2024-08-28 11:14:53 +0900

    static final

---
+++ src/Ch06/exercise/P17/Printer.java
@@ -0,0 +1,19 @@
+package Ch06.exercise.P17;
+
+public class Printer {
+    static void println(int a) {
+        System.out.println(a);
+    }
+
+    static void println(boolean b) {
+        System.out.println(b);
+    }
+
+    static void println(double d) {
+        System.out.println(d);
+    }
+
+    static void println(String s) {
+        System.out.println(s);
+    }
+}
---
+++ src/Ch06/exercise/P17/PrinterExample.java
@@ -0,0 +1,10 @@
+package Ch06.exercise.P17;
+
+public class PrinterExample {
+    public static void main(String[] args) {
+        Printer.println(3);
+        Printer.println(true);
+        Printer.println(3.14);
+        Printer.println("홍길동");
+    }
+}
