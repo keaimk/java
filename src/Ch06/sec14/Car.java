602a98e854ceb32f3dd18b9b0ff7e6a7ed7eacbb
keaimk <fly1043@naver.com>
2024-08-28 15:01:22 +0900

    access modifier

---
+++ src/Ch06/sec14/Car.java
@@ -0,0 +1,37 @@
+package Ch06.sec14;
+
+public class Car {
+    private int speed;
+    private boolean stop;
+
+    public int getSpeed() {
+        return speed;
+    }
+    public void setSpeed(int speed) {
+        if (speed < 0) {
+            this.speed = 0;
+        else {
+            this.speed;
+
+            public boolean is stop();
+            isStop() {
+                return stop;
+                }
+
+                public void stop;
+
+
+
+
+
+
+
+
+
+
+
+            ddddD
+            }
+        }
+    }
+}
