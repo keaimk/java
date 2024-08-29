2eeec6d17b53c87292ffc32d228f3d17e777e1ab
keaimk <fly1043@naver.com>
2024-08-29 10:00:58 +0900

    encapsulation

--- src/Ch06/sec14/CarExample.java
+++ src/Ch06/sec14/CarExample.java
@@ -14,6 +14,7 @@ public class CarExample {
         if (!myCar.isStop()) {
             myCar.setStop(true);
         }
-        System.out.println("현재 속도: " + myCar.getSpeed());
+        System.out.println("현재 속도: " + myCar.getSpeed()
+        );
     }
 }
