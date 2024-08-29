354b5262bd7a9d7f6d8fc15af89f1243cc4ed63e
keaimk <fly1043@naver.com>
2024-08-29 10:23:59 +0900

    파일 옮김

---
+++ src/Ch07/lecture/sec03/exam01/Phone.java
@@ -0,0 +1,10 @@
+package Ch07.lecture.sec03.exam01;
+
+public class Phone {
+    public String model;
+    public String color;
+
+    public Phone() {
+        System.out.println("Phone() 생성자 실행");
+    }
+}
