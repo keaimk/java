3cc05430c65fad0879b7d3e1a32bf5fa07379d08
keaimk <fly1043@naver.com>
2024-08-29 10:35:09 +0900

    override

---
+++ src/Ch07/lecture/sec04/exam01/Calculator.java
@@ -0,0 +1,8 @@
+package Ch07.lecture.sec04.exam01;
+
+public class Calculator {
+    public double areaCircle(double r) {
+        System.out.println("Calculator 객체의 areaCircle() 실행");
+        return 3.141519 * r * r;
+    }
+}
