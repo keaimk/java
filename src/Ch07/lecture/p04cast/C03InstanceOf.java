daf000c291d99e08a13f6d6c059111c30f80079f
keaimk <fly1043@naver.com>
2024-08-29 16:41:35 +0900

    instanceof

---
+++ src/Ch07/lecture/p04cast/C03InstanceOf.java
@@ -0,0 +1,27 @@
+package Ch07.lecture.p04cast;
+
+public class C03InstanceOf {
+    public static void main(String[] args) {
+        Parent3 a = new Child31();
+
+        if (a instanceof Child31) {
+            Child31 b = (Child31) a; // ok
+        }
+
+        if (a instanceof Child32) {
+            Child32 c = (Child32) a; // not ok
+        }
+
+        System.out.println("다음 코드");
+    }
+}
+
+// @formatter : off
+class Parent3 {
+}
+
+class Child31 extends Parent3 {
+}
+
+class Child32 extends Parent3 {
+}
