2807e2bae7f10d4c3f33f786f84033c60db802cf
keaimk <fly1043@naver.com>
2024-08-30 09:52:46 +0900

    abstract class

---
+++ src/Ch07/sec10/exam01/SmartPhone.java
@@ -0,0 +1,11 @@
+package Ch07.sec10.exam01;
+
+public class SmartPhone extends Phone {
+    SmartPhone(String owner) {
+        super(owner);
+    }
+
+    void internetSearch() {
+        System.out.println("인터넷을 검색합니다.");
+    }
+}
