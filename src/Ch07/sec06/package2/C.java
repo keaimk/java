27d3ee6b313eba2277784eee5be92a7b2f930004
keaimk <fly1043@naver.com>
2024-08-30 11:51:37 +0900

    protected

---
+++ src/Ch07/sec06/package2/C.java
@@ -0,0 +1,11 @@
+package Ch07.sec06.package2;
+
+import Ch07.sec06.package1.A;
+
+public class C {
+    public void method() {
+//        A a = new A();
+//        a.filed = "value";
+//        a.method();
+    }
+}
