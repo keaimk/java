5465cdccc6a1c61287a83ec58b05848a98026894
keaimk <fly1043@naver.com>
2024-09-03 09:52:48 +0900

    람다식의 메소드 몸통의 명령문이 한 줄이면 중괄호 생략 가능

---
+++ src/Ch16/lecture/p01lambda/C05MethodBody.java
@@ -0,0 +1,27 @@
+package Ch16.lecture.p01lambda;
+
+public class C05MethodBody {
+    public static void main(String[] args) {
+//        메소드 몸통(코드블록)내의
+//        명령문이 1줄이 아니면
+//        { } 붙여야 함
+        MyInterface5 o = () -> {
+            System.out.println("명령문1");
+            System.out.println("명령문2");
+        };
+        o.some();
+//        명령문이 1줄이면 {} 중괄호 생략 가능
+        MyInterface5 p = () -> {
+            System.out.println("한 줄");
+        };
+        p.some();
+
+        MyInterface5 q = () -> System.out.println("one line");
+        q.some();
+    }
+}
+
+@FunctionalInterface
+interface MyInterface5 {
+    void some();
+}
