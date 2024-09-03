8ab4ab02c721325c08fd8d9e5e050b3bf0dcf2f7
keaimk <fly1043@naver.com>
2024-09-03 15:51:18 +0900

    생성자 참조

---
+++ src/Ch16/sec05/exam03/ConstructorReferenceExample.java
@@ -0,0 +1,16 @@
+package Ch16.sec05.exam03;
+
+public class ConstructorReferenceExample {
+    public static void main(String[] args) {
+        Person person = new Person();
+
+        Member m1 = person.getMemeber1(Member::new);
+        System.out.println(m1);
+        System.out.println();
+
+        Member m2 = person.getMember2(Member::new);
+        System.out.println(m2);
+    }
+}
+
+
