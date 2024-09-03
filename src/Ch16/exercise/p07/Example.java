8cd6547be38158dcf762e6507ed86e351e56e427
keaimk <fly1043@naver.com>
2024-09-03 12:24:42 +0900

    확인문제 7 / Math.max, Math.min

---
+++ src/Ch16/exercise/p07/Example.java
@@ -0,0 +1,23 @@
+package Ch16.exercise.p07;
+
+public class Example {
+    private static int[] scores = {10, 50, 3};
+
+    public static int maxOrMin(Operator operator) {
+        int result = scores[0];
+
+        for (int score : scores) {
+            result = operator.apply(result, score);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int max = maxOrMin((x, y) -> x > y ? x : y); // 50
+        System.out.println("max = " + max);
+
+        int min = maxOrMin((a, b) -> a > b ? b : a); // 3
+        System.out.println("min = " + min);
+    }
+}
