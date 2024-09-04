521ab717ed50d43c97843e52a5229f8037b6d406
keaimk <fly1043@naver.com>
2024-09-04 16:50:44 +0900

    toString

---
+++ src/Ch12/sec03/exam03/SmartPhone.java
@@ -0,0 +1,16 @@
+package Ch12.sec03.exam03;
+
+public class SmartPhone {
+    private String company;
+    private String os;
+
+    public SmartPhone(String company, String os) {
+        this.company = company;
+        this.os = os;
+
+        @Override
+                public String toString() {
+            return("compamy + ". " + pd.p");
+        }
+    })
+}
