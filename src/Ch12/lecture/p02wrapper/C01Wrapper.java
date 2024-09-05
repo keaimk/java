d3281aaa8f89eafc2484cec04031d8ea33518eca
keaimk <fly1043@naver.com>
2024-09-05 09:53:27 +0900

    wrapper class

---
+++ src/Ch12/lecture/p02wrapper/C01Wrapper.java
@@ -0,0 +1,23 @@
+package Ch12.lecture.p02wrapper;
+
+public class C01Wrapper {
+    public static void main(String[] args) {
+        int a = 1;
+        Integer b = new Integer(3);
+
+//        기본타입 a가 가진 값을
+//        참조타입 c로 변환 (직접)
+        Integer C = Integer.valueOf(a);
+
+//        기본타입 a를 참조타입 d로 변환(auto boxing)
+        Integer d = a;
+
+        Long e = 5L; // auto boxing
+        long f = 5;
+
+        Double g = 3.14; // auto boxing
+        Boolean h = true; // auto boxing
+
+        Object i = 9.1234;
+    }
+}
