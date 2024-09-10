dd14baee3a47074984d7c9c87bdc62833f860da2
keaimk <fly1043@naver.com>
2024-09-10 12:37:03 +0900

    map

---
+++ src/Ch15/sec05/exam04/ComparatorExample.java
@@ -0,0 +1,17 @@
+package Ch15.sec05.exam04;
+
+import java.util.TreeSet;
+
+public class ComparatorExample {
+    public static void main(String[] args) {
+        TreeSet<Fruit> treeSet = new TreeSet<Fruit>(new FruitComparator());
+
+        treeSet.add(new Fruit("포도", 300));
+        treeSet.add(new Fruit("수박", 10000));
+        treeSet.add(new Fruit("딸기", 6000));
+
+        for (Fruit fruit : treeSet) {
+            System.out.println(fruit.name + ":" + fruit.price);
+        }
+    }
+}
