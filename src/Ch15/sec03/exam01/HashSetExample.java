98a6bd1ef35e87d8674c1a2e4d7b13236fc0847f
keaimk <fly1043@naver.com>
2024-09-10 17:50:21 +0900

    set

---
+++ src/Ch15/sec03/exam01/HashSetExample.java
@@ -0,0 +1,10 @@
+package Ch15.sec03.exam01;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetExample {
+    public static void main(String[] args) {
+        Set<String> set = new HashSet<>();
+    }
+}
