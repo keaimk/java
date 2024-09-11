368a94529699c76c1bb0ff5ea4ff08bd842c5fe3
keaimk <fly1043@naver.com>
2024-09-11 14:46:56 +0900

    stream

---
+++ src/Ch17/lecutre/p01stream/C01Stream.java
@@ -0,0 +1,12 @@
+package Ch17.lecutre.p01stream;
+
+public class C01Stream {
+    public static void main(String[] args) {
+
+//        1. 스트림 만들기
+
+//        2. 중간 연산
+
+//        3. 최종 연산
+    }
+}
