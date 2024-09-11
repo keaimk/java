4c4202051c1d8c10950c1d10d5d86e43ad3522e3
keaimk <fly1043@naver.com>
2024-09-11 16:30:57 +0900

    중간 연산, 최종 연산

---
+++ src/Ch17/lecutre/p02operation/C01Operation.java
@@ -0,0 +1,12 @@
+package Ch17.lecutre.p02operation;
+
+public class C01Operation {
+    public static void main(String[] args) {
+//        스트림 연산(메소드)
+
+//        1. 중간 연산(intermediate operation)
+//           return : Stream(IntStream, DoubleStream, LongStream)
+
+//        2. 최종 연산(terminal operation)
+    }
+}
