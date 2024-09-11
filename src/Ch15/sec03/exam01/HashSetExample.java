c3d6095fe05eb2908d99a70669700b9c2d79cd66
keaimk <fly1043@naver.com>
2024-09-11 09:53:17 +0900

    set

--- src/Ch15/sec03/exam01/HashSetExample.java
+++ src/Ch15/sec03/exam01/HashSetExample.java
@@ -6,5 +6,14 @@ import java.util.Set;
 public class HashSetExample {
     public static void main(String[] args) {
         Set<String> set = new HashSet<>();
+
+        set.add("Java");
+        set.add("JDBC");
+        set.add("JSP");
+        set.add("Java");
+        set.add("Spring");
+
+        int size = set.size();
+        System.out.println("총 객체 수: " + size);
     }
 }
