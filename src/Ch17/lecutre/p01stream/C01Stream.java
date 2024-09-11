4cbf979d7ff83acdc9d8318c855d40903a0563cb
keaimk <fly1043@naver.com>
2024-09-11 14:48:00 +0900

    stream

--- src/Ch17/lecutre/p01stream/C01Stream.java
+++ src/Ch17/lecutre/p01stream/C01Stream.java
@@ -2,11 +2,16 @@ package Ch17.lecutre.p01stream;
 
 public class C01Stream {
     public static void main(String[] args) {
+        // stream : 흘러가는 데이터에 대한 연산(메소드)을 가진 객체
 
-//        1. 스트림 만들기
+        // package : java.util.stream
+        // Stream, IntStream, DoubleStream, LongStream
+        //
 
-//        2. 중간 연산
+        // 1. 스트림 만들기
 
-//        3. 최종 연산
+        // 2. 중간연산
+
+        // 3. 최종연산
     }
 }
