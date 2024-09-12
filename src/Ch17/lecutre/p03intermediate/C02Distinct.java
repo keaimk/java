7db26bdbb37d08de33e7bb9f179762a2d52fd6bd
keaimk <fly1043@naver.com>
2024-09-12 09:45:17 +0900

    distinct, filter

--- src/Ch17/lecutre/p03intermediate/C02Distinct.java
+++ src/Ch17/lecutre/p03intermediate/C02Distinct.java
@@ -2,7 +2,7 @@ package Ch17.lecutre.p03intermediate;
 
 import java.util.List;
 
-public class Distinct {
+public class C02Distinct {
     public static void main(String[] args) {
 //        distinct : 중복 제거
         List<Integer> list = List.of(3, 1, 4, 1, 5, 9, 2);
