9ad1f2f597dbf47731fbfa75895d5743644cd6d9
keaimk <fly1043@naver.com>
2024-09-13 11:37:28 +0900

    try catch

---
+++ src/Ch11/lecture/p02try/C03Try.java
@@ -0,0 +1,20 @@
+package Ch11.lecture.p02try;
+
+public class C03Try {
+    public static void main(String[] args) {
+        try {
+            int a = 5;
+            int b = 2;
+
+            int c = a / b; // Exception 발생
+
+            System.out.println("c = " + c);
+        } catch (Exception e) {
+//            catch 블록 내의 코드들 ..
+            e.printStackTrace();
+        }
+
+//        다음 실행 흐름 ..
+        System.out.println("다음 코드들");
+    }
+}
