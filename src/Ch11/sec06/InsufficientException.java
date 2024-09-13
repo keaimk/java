b7a7ffb830bda4e62e68e958cda13eece00124db
keaimk <fly1043@naver.com>
2024-09-13 17:37:35 +0900

    throw

---
+++ src/Ch11/sec06/InsufficientException.java
@@ -0,0 +1,11 @@
+package Ch11.sec06;
+
+public class InsufficientException extends Exception {
+    public InsufficientException() {
+
+    }
+
+    public InsufficientException(String message) {
+        super(message);
+    }
+}
