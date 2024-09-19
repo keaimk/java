3a938ee788dfdd69df0f37584355207bff3b393a
keaimk <fly1043@naver.com>
2024-09-19 15:51:21 +0900

    try-with-resources

---
+++ src/Ch18/lecture/p1io/C04OutputStream.java
@@ -0,0 +1,34 @@
+package Ch18.lecture.p1io;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class C04OutputStream {
+    public static void main(String[] args) {
+
+        String name = "C:/Windows/Temp/output4.txt";
+        // try-with-resources
+        try (
+                OutputStream os = new FileOutputStream(name);
+                InputStream is = new FileInputStream("");
+                Scanner scanner = new Scanner("");
+        ) {
+
+            os.write(99);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        try {
+            OutputStream os = new FileOutputStream("");
+            InputStream is = new FileInputStream("");
+            Scanner scanner = new Scanner("");
+            try (os; is; scanner) {
+                // os, is, scanner로 작업
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
