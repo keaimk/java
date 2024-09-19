3ae1db24b528158057eee3832cb068fe4bf61a6a
keaimk <fly1043@naver.com>
2024-09-19 11:12:45 +0900

    syncronized

---
+++ src/Ch14/lecture/p3synchronized/C02Thread.java
@@ -0,0 +1,30 @@
+package Ch14.lecture.p3synchronized;
+
+public class C02Thread {
+    private static int value = 0;
+
+    private static Object o = new Object(); // lock, monitor
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 10000; i++) {
+                synchronized (o) {
+                    value++;
+                }
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 10000; i++) {
+                synchronized (o) {
+                    value++;
+                }
+            }
+        });
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        System.out.println("value = " + value);
+    }
+}
