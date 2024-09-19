cfcca3bd363fabfc73926d5f8931299b7940245b
keaimk <fly1043@naver.com>
2024-09-19 15:57:33 +0900

    write

--- src/Ch18/lecture/p1io/C02OutputStream.java
+++ src/Ch18/lecture/p1io/C02OutputStream.java
@@ -6,12 +6,14 @@ import java.io.OutputStream;
 
 public class C02OutputStream {
     public static void main(String[] args) throws IOException {
-        String name = "C:/Windows/Temp/output2.txt";
+        String name = "C:/Temp/output2.txt";
         OutputStream os = new FileOutputStream(name);
 
         // int : 4 bytes -21억~21억
         // byte : 1 byte (-128~127)
         os.write(-128);
         os.write(201234567);
+
+
     }
 }
