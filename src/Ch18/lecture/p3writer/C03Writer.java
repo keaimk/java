52fbb31809d10a2bdcfbcdf5f10321f0b179557e
keaimk <fly1043@naver.com>
2024-09-20 10:19:05 +0900

    writer

---
+++ src/Ch18/lecture/p3writer/C03Writer.java
@@ -0,0 +1,35 @@
+package Ch18.lecture.p3writer;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Scanner;
+
+public class C03Writer {
+    public static void main(String[] args) {
+        String name = "C:/Temp/output33.txt";
+
+        try {
+            Scanner scanner = new Scanner(System.in);
+            Writer writer = new FileWriter(name);
+
+            try (scanner; writer) {
+                while (true) {
+                    System.out.print("입력>");
+                    String line = scanner.nextLine();
+
+                    if (line.equals("종료")) {
+                        break;
+                    }
+                    writer.write(line);
+                    writer.write('\n');
+                }
+            }
+
+            String line = scanner.nextLine();
+            writer.write(line);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
