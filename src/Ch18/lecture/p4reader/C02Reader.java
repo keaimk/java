93f80a004df10fda7745d9543c2fc3f52193ebdd
keaimk <fly1043@naver.com>
2024-09-20 10:41:18 +0900

    reader

---
+++ src/Ch18/lecture/p4reader/C02Reader.java
@@ -0,0 +1,23 @@
+package Ch18.lecture.p4reader;
+
+import java.io.FileReader;
+import java.io.Reader;
+import java.util.Arrays;
+
+public class C02Reader {
+    public static void main(String[] args) throws Exception {
+        String name = "C:/Temp/output33.txt";
+        Reader reader = new FileReader(name);
+
+        char[] data = new char[5];
+        System.out.println(Arrays.toString(data));
+
+        reader.read(data);
+        System.out.println(Arrays.toString(data));
+
+        reader.read(data);
+        System.out.println(Arrays.toString(data));
+
+        reader.close();
+    }
+}
