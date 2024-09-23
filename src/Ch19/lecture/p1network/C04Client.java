eefd0599c4387b8d754d37ff4995f50390b87281
keaimk <fly1043@naver.com>
2024-09-23 11:36:24 +0900

    network

--- src/Ch19/lecture/p1network/C04Client.java
+++ src/Ch19/lecture/p1network/C04Client.java
@@ -6,23 +6,23 @@ import java.io.FileInputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 
-public class C03Client {
+public class C04Client {
     public static void main(String[] args) throws Exception {
 
-        Socket socket = new Socket("172.30.1.29", 9999);
+        Socket socket = new Socket("172.30.1.84", 9999);
 
         OutputStream os = socket.getOutputStream();
         BufferedOutputStream bos = new BufferedOutputStream(os);
 
-        String fileName = "C:/Temp/지구온난화.jpg";
+        String fileName = "C:/Temp/강아지.jpg";
         FileInputStream fis = new FileInputStream(fileName);
         BufferedInputStream bis = new BufferedInputStream(fis);
 
-        try (bis; fis; bos; os; socket;) {
+        try (bos; os; bis; socket;) {
 
             byte[] data = new byte[1000];
             int length = 0;
-
+            
             while ((length = bis.read(data)) != -1) {
                 bos.write(data, 0, length);
             }
